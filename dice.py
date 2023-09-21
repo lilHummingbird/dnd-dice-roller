3ecd79019ba623ccddafc4d8a518e9c8af68a09a
Hummingbird <stphpedersen@gmail.com>
2023-09-21 17:12:28 +0200

    Complete

--- dice.py
+++ dice.py
@@ -5,36 +5,37 @@
 #
 import random
 
- def d2():
-     roll = random.randint(1,2)
-     return roll
+def d2():
+    roll = random.randint(1,2)
+    return roll
  
- def d4():
-     roll = random.randint(1,4)
-     return roll
+def d4():
+    roll = random.randint(1,4)
+    return roll
 
- def d6():
-     roll = random.randint(1,6)
-     return roll
+def d6():
+    roll = random.randint(1,6)
+    return roll
 
- def d8():
-     roll = random.randint(1,8)
-     return roll 
+def d8():
+    roll = random.randint(1,8)
+    return roll 
  
- def d10():
-     roll = random.randint(1,10)
-     return roll
+def d10():
+    roll = random.randint(1,10)
+    return roll
 
- def dper():
-     roll = random.randint(1,100)
-     return roll
+def dper():
+    roll = random.randint(1,100)
+    return roll
 
- def d12():
-     roll = random.randint(1,12)
-     return roll
+def d12():
+    roll = random.randint(1,12)
+    return roll
 
- def d20():
-     roll = random.randint(1,20)
-     return roll
+def d20():
+    roll = random.randint(1,20)
+    return roll
+
 
 
