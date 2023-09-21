0475a6415d8402cb0a73ba9eee15db1e9322968f
Hummingbird <stphpedersen@gmail.com>
2023-09-21 16:37:47 +0200

    Complete

--- dice.py
+++ dice.py
@@ -3,3 +3,38 @@
 #
 # Created by Persephone on 21.09.23 at 16.07
 #
+import random
+
+ def d2():
+     roll = random.randint(1,2)
+     return roll
+ 
+ def d4():
+     roll = random.randint(1,4)
+     return roll
+
+ def d6():
+     roll = random.randint(1,6)
+     return roll
+
+ def d8():
+     roll = random.randint(1,8)
+     return roll 
+ 
+ def d10():
+     roll = random.randint(1,10)
+     return roll
+
+ def dper():
+     roll = random.randint(1,100)
+     return roll
+
+ def d12():
+     roll = random.randint(1,12)
+     return roll
+
+ def d20():
+     roll = random.randint(1,20)
+     return roll
+
+
